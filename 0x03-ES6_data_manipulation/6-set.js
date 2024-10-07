c59872d68a4696c70e884d3dcca44468b19cdbd8
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-10-07 21:57:36 +0100

    added 6-set.js 7-has_array_values.js 8-clean_set.js 9-groceries_list.js

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Nwaegunwa Emmanuel <https://github.com/Maxzeno>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
